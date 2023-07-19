d72eae5033c354aa4a27e9e87990ac612a84cc49
Eugene <eugene@Eugenes-MacBook-Pro.local>
2023-07-19 16:56:00 +0800

    Added new files

--- main.tf
+++ main.tf
@@ -1,51 +1,157 @@
-# Resource group name is output when execution plan is applied.
-resource "azurerm_resource_group" "sh" {
-  name     = var.rg_name
+resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
+  name     = "${random_pet.prefix.id}-rg"
 }
 
-# Create AVD workspace
-resource "azurerm_virtual_desktop_workspace" "workspace" {
-  name                = var.workspace
-  resource_group_name = azurerm_resource_group.sh.name
-  location            = azurerm_resource_group.sh.location
-  friendly_name       = "${var.prefix} Workspace"
-  description         = "${var.prefix} Workspace"
+# Create virtual network
+resource "azurerm_virtual_network" "my_terraform_network" {
+  name                = "${random_pet.prefix.id}-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
-# Create AVD host pool
-resource "azurerm_virtual_desktop_host_pool" "hostpool" {
-  resource_group_name      = azurerm_resource_group.sh.name
-  location                 = azurerm_resource_group.sh.location
-  name                     = var.hostpool
-  friendly_name            = var.hostpool
-  validate_environment     = true
-  custom_rdp_properties    = "audiocapturemode:i:1;audiomode:i:0;"
-  description              = "${var.prefix} Terraform HostPool"
-  type                     = "Pooled"
-  maximum_sessions_allowed = 16
-  load_balancer_type       = "DepthFirst" #[BreadthFirst DepthFirst]
+# Create subnet
+resource "azurerm_subnet" "my_terraform_subnet" {
+  name                 = "${random_pet.prefix.id}-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.my_terraform_network.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_virtual_desktop_host_pool_registration_info" "registrationinfo" {
-  hostpool_id     = azurerm_virtual_desktop_host_pool.hostpool.id
-  expiration_date = var.rfc3339
+# Create public IPs
+resource "azurerm_public_ip" "my_terraform_public_ip" {
+  name                = "${random_pet.prefix.id}-public-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
 }
 
-# Create AVD DAG
-resource "azurerm_virtual_desktop_application_group" "dag" {
-  resource_group_name = azurerm_resource_group.sh.name
-  host_pool_id        = azurerm_virtual_desktop_host_pool.hostpool.id
-  location            = azurerm_resource_group.sh.location
-  type                = "Desktop"
-  name                = "${var.prefix}-dag"
-  friendly_name       = "Desktop AppGroup"
-  description         = "AVD application group"
-  depends_on          = [azurerm_virtual_desktop_host_pool.hostpool, azurerm_virtual_desktop_workspace.workspace]
+# Create Network Security Group and rules
+resource "azurerm_network_security_group" "my_terraform_nsg" {
+  name                = "${random_pet.prefix.id}-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "RDP"
+    priority                   = 1000
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "web"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
-# Associate Workspace and DAG
-resource "azurerm_virtual_desktop_workspace_application_group_association" "ws-dag" {
-  application_group_id = azurerm_virtual_desktop_application_group.dag.id
-  workspace_id         = azurerm_virtual_desktop_workspace.workspace.id
+# Create network interface
+resource "azurerm_network_interface" "my_terraform_nic" {
+  name                = "${random_pet.prefix.id}-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "my_nic_configuration"
+    subnet_id                     = azurerm_subnet.my_terraform_subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.my_terraform_public_ip.id
+  }
+}
+
+# Connect the security group to the network interface
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.my_terraform_nic.id
+  network_security_group_id = azurerm_network_security_group.my_terraform_nsg.id
+}
+
+# Create storage account for boot diagnostics
+resource "azurerm_storage_account" "my_storage_account" {
+  name                     = "diag${random_id.random_id.hex}"
+  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+
+# Create virtual machine
+resource "azurerm_windows_virtual_machine" "main" {
+  name                  = "${var.prefix}-vm"
+  admin_username        = "azureuser"
+  admin_password        = random_password.password.result
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.my_terraform_nic.id]
+  size                  = "Standard_DS1_v2"
+
+  os_disk {
+    name                 = "myOsDisk"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2022-datacenter-azure-edition"
+    version   = "latest"
+  }
+
+
+  boot_diagnostics {
+    storage_account_uri = azurerm_storage_account.my_storage_account.primary_blob_endpoint
+  }
+}
+
+# Install IIS web server to the virtual machine
+resource "azurerm_virtual_machine_extension" "web_server_install" {
+  name                       = "${random_pet.prefix.id}-wsi"
+  virtual_machine_id         = azurerm_windows_virtual_machine.main.id
+  publisher                  = "Microsoft.Compute"
+  type                       = "CustomScriptExtension"
+  type_handler_version       = "1.8"
+  auto_upgrade_minor_version = true
+
+  settings = <<SETTINGS
+    {
+      "commandToExecute": "powershell -ExecutionPolicy Unrestricted Install-WindowsFeature -Name Web-Server -IncludeAllSubFeature -IncludeManagementTools"
+    }
+  SETTINGS
+}
+
+# Generate random text for a unique storage account name
+resource "random_id" "random_id" {
+  keepers = {
+    # Generate a new ID only when a new resource group is defined
+    resource_group = azurerm_resource_group.rg.name
+  }
+
+  byte_length = 8
+}
+
+resource "random_password" "password" {
+  length      = 20
+  min_lower   = 1
+  min_upper   = 1
+  min_numeric = 1
+  min_special = 1
+  special     = true
+}
+
+resource "random_pet" "prefix" {
+  prefix = var.prefix
+  length = 1
 }
